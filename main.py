36f2627d56fd7eb0df1560a3be705b9d8dd40c23
sondrtha <sondre_H_93@hotmail.com>
2022-02-23 20:52:01 +0100

    added longer test

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ def test_1():
     assert(x < 200)
 
 
-def test_2_fail():
+def test_2():
     x = 10
-    assert x == 12
+    assert x == 10
+
+
+def test_3(): 
+	sum = 0
+	for i in range(100000):
+		sum += i
+	assert(sum > 10000000)
+
+
 
 
